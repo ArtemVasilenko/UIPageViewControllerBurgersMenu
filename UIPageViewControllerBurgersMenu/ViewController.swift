4aa9b2d522988c6661e774916026c83207c20150
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-21 13:16:54 +0200

    Initial Commit

---
+++ UIPageViewControllerBurgersMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageViewControllerBurgersMenu
+//
+//  Created by Артем on 3/21/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
